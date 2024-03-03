66a4d3bfea2b6640706047631891c1fc24152272
kevinchacha <kevinchacha29@yahoo.com>
2024-03-02 20:58:11 -0500

    Done Structure

---
+++ hoteleria_ec/src/main/java/com/kacm/hoteleria_ec/HoteleriaEcApplication.java
@@ -0,0 +1,13 @@
+package com.kacm.hoteleria_ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteleriaEcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteleriaEcApplication.class, args);
+	}
+
+}
---
+++ hoteleria_ec/src/test/java/com/kacm/hoteleria_ec/HoteleriaEcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kacm.hoteleria_ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteleriaEcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
